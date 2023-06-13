d847c5f5f0dbd46f9d5ee6f81fae7ebf4594222d
abrarzahed <abrarzahed6986@gmail.com>
2023-06-13 15:57:37 +0600

    binary search

--- app.js
+++ app.js
@@ -117,3 +117,26 @@ function doLinearSearch(arr, n) {
 // Big-o = O(n)
 console.log(doLinearSearch([1, 2, 3, 4, 5], 5));
 */
+
+/* 
+  COMMENT: binary search
+*/
+/*
+function doBinarySearch(arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    } else if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else if (target > arr[middleIndex]) {
+      leftIndex = middleIndex + 1;
+    }
+  }
+}
+// Big-o = O(logn)
+console.log(doBinarySearch([1, 2, 3, 4, 5, 6], 2));
+*/
