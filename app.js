d7cac8f252a33e0dac7e6cebe7bcced4cf1a2799
abrarzahed <abrarzahed6986@gmail.com>
2023-06-14 12:42:26 +0600

    bubble sort

--- app.js
+++ app.js
@@ -140,3 +140,26 @@ function doBinarySearch(arr, target) {
 // Big-o = O(logn)
 console.log(doBinarySearch([1, 2, 3, 4, 5, 6], 2));
 */
+
+/****************************************** 
+COMMENT: sorting algorithm 
+******************************************/
+/*
+function doBubbleSort(arr) {
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+}
+// Big-o = O(n^2)
+console.log(doBubbleSort([2, 1 - 2, 5, 3, -5]));
+*/
