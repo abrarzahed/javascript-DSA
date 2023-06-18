f26d64d264e8d77eb3b30771b58ef4efef5374c3
abrarzahed <abrarzahed6986@gmail.com>
2023-06-18 21:54:13 +0600

    circular queue

--- app.js
+++ app.js
@@ -367,3 +367,27 @@ queue2.dequeue();
 queue2.print();
 console.log(queue2.peak());
 */
+
+/****************************************** 
+COMMENT: circular queue implementation   
+******************************************/
+/*
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.rear = -1;
+    this.front = -1;
+  }
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+  isEmpty() {
+    return this.currentLength === 0;
+  }
+  enqueue(){
+
+  }
+}
+*/
